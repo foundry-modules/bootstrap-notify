edc4ec6f3709d7162eb063ce9dc7ac6606a75684
Jason Ramos <jason@stackideas.com>
2012-11-16 14:55:13 +0800

    Use $ instead of window.jQuery.

--- js/bootstrap-notify.js
+++ js/bootstrap-notify.js
@@ -37,13 +37,13 @@
 
     if(!this.options.message && this.$element.data("message") !== '') // dom text
       this.$note.html(this.$element.data("message"));
-    else 
+    else
       if(typeof this.options.message === 'object')
         if(this.options.message.html)
           this.$note.html(this.options.message.html);
         else if(this.options.message.text)
           this.$note.text(this.options.message.text);
-      else 
+      else
         this.$note.html(this.options.message);
 
     if(this.options.closable)
@@ -90,4 +90,4 @@
     onClose: function () {},
     onClosed: function () {}
   }
-}(window.jQuery);
+}($);
